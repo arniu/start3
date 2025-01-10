73ba2f116526f517e0b97711ebaa01bebd1b0ae9
arniu <arniu2006@163.com>
2025-01-10 11:04:11 +0800

    [starter] Add orbit-controls

--- src/app/starter/page.tsx
+++ src/app/starter/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { OrbitControls } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { useRef, useState } from "react";
 import * as THREE from "three";
@@ -9,11 +10,18 @@ export default function Page() {
   return (
     <div className="canvas-container">
       <Canvas>
-        <ambientLight intensity={0.5} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-        <pointLight position={[-10, -10, -10]} />
+        <ambientLight intensity={Math.PI / 2} />
+        <spotLight
+          position={[10, 10, 10]}
+          angle={0.15}
+          penumbra={1}
+          decay={0}
+          intensity={Math.PI}
+        />
+        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
         <Box position={[-1.2, 0, 0]} />
         <Box position={[1.2, 0, 0]} />
+        <OrbitControls />
       </Canvas>
     </div>
   );
